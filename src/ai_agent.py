ae0652ca68497efd39d3d76e7dd2aa58f067bc2d
Enver Emirhan Aktürk <enver649@MacBook-Air-von-Enver.local>
2026-09-11 23:31:17 +0300

    Implement summarize_text function with Groq API

---
+++ src/ai_agent.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+
+client = Groq()
+def summarize_text(text):
+    chat_completion = client.chat.completions.create(
+    messages=[
+        {
+              "role": "user",
+             "content": f"Fasse folgenden Text zusammen:\n\n{text}",
+        }
+        ],
+        model="groq/compound-mini",
+        max_tokens=150,
+    )
+    return chat_completion.choices[0].message.content
+
+ergebnis = summarize_text("Python ist eine Programmiersprache fuer Webentwicklung und KI.")
+print("Ergebnis ist:", ergebnis)
